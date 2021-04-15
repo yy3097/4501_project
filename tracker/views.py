3b28004f078542df12ec96978d1c2debe23772a1
rx <rx2187@columbia.edu>
2021-04-15 19:45:24 -0400

    finish add locs of sightings to the map

--- tracker/views.py
+++ tracker/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from tracker.models import Sighting
 # Create your views here.
 
 def home(request):
     return render(request,'tracker/home.html',{})
 
 def map(request):
-    return render(request, 'tracker/map.html', {})
+    sightings = Sighting.objects.all()[:100]
+    locs = []
+    for sighting in sightings:
+        loc = {"longitude": sighting.longitude, "latitude": sighting.latitude}
+        locs.append(loc)
+    return render(request, 'tracker/map.html', {'sightings': locs})
